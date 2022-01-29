e4e9bba82a69c9ccb05696c791ac6a28023df219
Nderim Saliu <nderim5saliu@gmail.com>
2022-01-29 19:14:05 +0100

    Add project files.

---
+++ WebApiDalogTask.Data/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace WebApiDalogTask.Data
+{
+    public class Class1
+    {
+    }
+}
---
+++ WebApiDalogTask.Services/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace WebApiDalogTask.Services
+{
+    public class Class1
+    {
+    }
+}
