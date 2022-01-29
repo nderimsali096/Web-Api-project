1baf4f231f382859a3fc130eabfb5e55ccdcc902
Nderim Saliu <nderim5saliu@gmail.com>
2022-01-29 21:33:36 +0100

    Created the dbContext and the model for users.

---
+++ WebApiDalogTask.Data/DalogDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using WebApiDalogTask.Data.Models;
+
+namespace WebApiDalogTask.Data
+{
+    public class DalogDbContext : IdentityDbContext
+    {
+        public DalogDbContext() { }
+        public DalogDbContext(DbContextOptions options) : base(options) { }
+
+        public virtual DbSet<User> Users { get; set; }
+
+    }
+}
---
+++ WebApiDalogTask.Data/Models/User.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WebApiDalogTask.Data.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public String Name { get; set; }
+        public String Email { get; set; }
+        public int CompanyId { get; set; }
+
+    }
+}
